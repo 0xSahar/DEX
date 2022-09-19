4a152ead8af5d5590065e8051afd395538aaa80e
0xSahar <Bztk@proton.me>
2022-09-19 08:32:21 -0700

    10.withdraw tokens

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -12,6 +12,7 @@ contract Exchange{
 	mapping (address => mapping(address => uint256)) public tokens;
 
 	event Deposit(address token , address user , uint256 amount , uint256 balance);
+	event Withdraw(address token , address user , uint256 amount , uint256 balance);
 
 	constructor(address _feeAccount , uint256 _feePercent){
        feeAccount = _feeAccount;
@@ -21,7 +22,7 @@ contract Exchange{
 
 
     // ------------------------------
-	// Deposit Tokens
+	// Deposit & Withdraw Tokens
 	function depositToken(address _token , uint256 _amount) public{
 	   // Transfer Tokens to exchange 
        require(Token(_token).transferFrom(msg.sender , address(this) , _amount));  
@@ -29,6 +30,18 @@ contract Exchange{
        tokens[_token][msg.sender]= tokens[_token][msg.sender] + _amount;
        //Emit an event
        emit Deposit(_token , msg.sender , _amount , tokens[_token][msg.sender] );  
+	}
+
+	function withdrawToken(address _token , uint256 _amount) public{
+		// ensure that user has enough tokens to withdraw
+		require(tokens[_token][msg.sender] >= _amount);
+		// Transfer tokens to the user
+		Token(_token).transfer(msg.sender , _amount);
+		// update balance
+		tokens[_token][msg.sender] = tokens[_token][msg.sender] - _amount;
+		// Emit an event
+		emit Withdraw(_token , msg.sender , _amount , tokens[_token][msg.sender]);
+
 	}
 
 	// check balances (wrapper funcction that checks the value out of a mapping)
