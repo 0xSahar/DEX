76e5dcdddf81f66989ef8d9a445013553a6ce739
0xSahar <Bztk@proton.me>
2022-11-25 07:11:46 -0800

    28.my transactions

--- src/store/Selectors.js
+++ src/store/Selectors.js
@@ -193,6 +193,64 @@ return({
 
  }
 
+ //----------------------------
+ // My Filled Orders
+
+ export const myFilledOrdersSelector = createSelector(
+ 	 account,
+ 	 filledOrders , 
+ 	 tokens, 
+ 	 (account ,orders , tokens) =>{
+
+ 	 	if(! tokens[0] || ! tokens[1]) { return }
+
+	  //filter orders created or filled  by current account
+	  orders = orders.filter((o)=> o.user === account || o.creator === account )	
+
+	  //filter orders by selected tokens
+    orders = orders.filter((o) => o.tokenGet === tokens[0].address || o.tokenGet === tokens[1].address)
+    orders = orders.filter((o) => o.tokenGive === tokens[0].address || o.tokenGive === tokens[1].address)
+
+    // sort orders by time descending
+     orders = orders.sort((a,b)=> b.timestamp - a.timestamp)
+
+     // decorate orders - add display attributes
+     orders = decorateMyFilledOrders(orders , account , tokens)
+
+    return orders
+    
+ })
+
+ const decorateMyFilledOrders =(orders , account , tokens) =>{
+ 	return(
+ 		orders.map((order) =>{
+ 			order = decorateOrder(order , tokens)
+ 			order = decorateMyfilledOrder(order , account , tokens)
+
+ 			return(order)
+ 		})
+ 	)
+ }
+
+const decorateMyfilledOrder =(order, account , tokens) =>{
+	const myOrder = order.creator === account
+
+	let orderType
+
+	if(myOrder){
+		orderType = (order.tokenGive === tokens[1].address ? 'buy' : 'sell')
+
+	}else{
+		orderType = (order.tokenGive === tokens[1].address ? 'sell' : 'buy')
+
+	}
+	return({
+		...order,
+		orderType,
+		orderClass : (orderType === 'buy' ? GREEN : RED) , 
+		orderSign : (orderType === 'buy' ? '+' : '-')
+	})
+}
 
 //----------------------------------
 //Order Book
